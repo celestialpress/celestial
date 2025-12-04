b6e43a3a826525bf89ecebe8ae4bf7b2a31f93cf
JRDN <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2025-12-04 04:16:57 +0000

    css fix + anti-deledao

--- README.md
+++ README.md
@@ -13,4 +13,4 @@
 
 ## Credits:
 - Coding4Hours & JustJxrdanWasDefinetlyTaken - site
-- soapphia - tinyjet
+- MercuryWorkshop - Scramjet
---
+++ public/news/cloak.js
@@ -0,0 +1,20 @@
+document.title = "celestial. | games";
+
+document.open();
+document.write(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>celestial. | games</title>
+    <link rel="shortcut icon" type="image/png" href="/assets/img/logo.png" />
+    <link rel="stylesheet" href="/assets/css/home.css" />
+</head>
+<body>
+    <input placeholder="search 600+ games." />
+    <div class="gs"></div>
+</body>
+</html>
+`);
+document.close();
