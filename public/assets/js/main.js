3d767ccb9f8ae9e70864222d3648e5bfb5b31311
JRDN <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2025-12-09 04:53:57 +0000

    homepage should be completely done, added tools directory

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,6 +5,7 @@ const url = document.querySelector('.url');
 const card = document.querySelector('#card');
 const card2 = document.querySelector('#card2');
 const card3 = document.querySelector('#card3');
+const card4 = document.querySelector('#card4');
 const foot = document.querySelector('.footer .a');
 const foot2 = document.querySelector('.footer .a2');
 const foot3 = document.querySelector('.footer .a3');
@@ -14,6 +15,8 @@ url.placeholder = "search with ease";
 card.querySelector('p').innerHTML = "ga<span>m</span>es";
 card2.querySelector('p').textContent = "chat";
 card3.querySelector('p').textContent = "media";
+// i know this is stupid but it looks really ugly when looking in the code so im doing this
+card4.querySelector('p').textContent = "tools";
 foot.textContent = "settings";
 foot2.innerHTML = "dis<span>c</span>ord";
 foot2.href = "https://dsc.gg/gnetwork";
--- public/news/cloak.js
+++ public/news/cloak.js
@@ -4,19 +4,34 @@ document.open();
 document.write(`
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>celestial. | games</title>
-    <link rel="shortcut icon" type="image/png" href="/assets/img/logo.png" />
-    <link rel="stylesheet" href="/assets/css/home.css" />
-    <link rel="stylesheet" href="/assets/css/edugs.css" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>celestial. | games</title>
+  <link rel="shortcut icon" type="image/png" href="/assets/img/logo.png" />
+  <link rel="stylesheet" href="/assets/css/home.css" />
+  <link rel="stylesheet" href="/assets/css/edugs.css" />
 </head>
+
 <body>
-    <input class="textbook" placeholder="search 600+ games." />
-    <div class="gs"></div>
+<div class="search">
+  <input class="textbook" placeholder="search 600+ games." />
+
+    <select id="choices">
+      <option id="all" selected>All G<span>am</span>es</option>
+      <option id="exclusive">Exclusives</option>
+    </select>
+
+    <select id="choices">
+      <option selected>In Order</option>
+      <option>Newest</option>
+    </select>
+  </div>
+  <div class="gs"></div>
 </body>
 <script src="/assets/js/newscards.js"></script>
+
 </html>
 `);
 document.close();
