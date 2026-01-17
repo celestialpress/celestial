9a6970f8b1b567d03eef0249cb5f1cd0459b346b
JRDN <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2026-01-17 02:53:43 +0000

    (i think) i got the transports working

---
+++ public/assets/js/prox.js
@@ -0,0 +1,27 @@
+// lmao ion feel like making this support modules so imma just import and change
+const pr0xySelect = document.getElementById('pr0xySelect');
+const transportsele = document.getElementById('tselect');
+
+pr0xySelect.addEventListener('change', () => {
+    localStorage.setItem('pr0xy', pr0xySelect.value);
+    location.reload();
+});
+
+transportsele.addEventListener('change', () => {
+    localStorage.setItem('transportz', transportsele.value);
+    location.reload();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedProxy = localStorage.getItem('pr0xy');
+    if (savedProxy && [...pr0xySelect.options].some(opt => opt.value === savedProxy)) {
+        pr0xySelect.value = savedProxy;
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTransport = localStorage.getItem('transportz');
+    if (savedTransport && [...transportsele.options].some(opt => opt.value === savedTransport)) {
+        transportsele.value = savedTransport;
+    }
+});
