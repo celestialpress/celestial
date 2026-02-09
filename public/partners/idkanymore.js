4f5721843d7388879d4b71db3fd7f81db139ba28
JRDN <108704541+JustJxrdanWasDefinetlyTaken@users.noreply.github.com>
2026-02-09 08:33:54 +0000

    added 4 more themes and new loading screen + partners page

--- public/partners/idkanymore.js
+++ public/partners/idkanymore.js
@@ -13,13 +13,65 @@ document.write(`
     <meta content="Drew's Learning Academy | Edison, NJ" property="og:title">
     <meta
         content="DLA, short for Drew's Learning Academy, is a new upcoming academy in edison, new jersey. DLA focuses on student success for K-12. We believe that the best academic dicipline is learning. Join now to set your child up for success.">
+        <style>
+        body {
+        margin: 0;
+        }
+        img {
+        width:200px;
+        border-radius:8px;
+        transition:0.5s;
+        }
+        img:hover {
+        transform:scale(1.05);
+        }
+
+        .pluh {
+        height:100vh;
+        width:100vw;
+        position:relative;
+        overflow:hidden;
+        }
+
+        .gridthing {
+    width: 100%;
+    height: 100%;
+    background:
+        repeating-linear-gradient(60deg,
+            #fff3 0 1px,
+            transparent 1px 40px),
+        repeating-linear-gradient(-60deg,
+            #fff3 0 1px,
+            transparent 1px 40px);
+            top:0;
+            right:0;
+            left:0;
+            overflow:hidden;
+}
+
+body[theme="light"] .gridthing {
+    background:
+        repeating-linear-gradient(60deg,
+            #0002 0 1px,
+            transparent 1px 40px),
+        repeating-linear-gradient(-60deg,
+            #0002 0 1px,
+            transparent 1px 40px);
+}
+        </style>
 </head>
 
 <body theme="default">
 <div align="center">
-<h1 id="titlething" style="font-weight:500;">SLQNT IS IN THE FILES</h1>
+<div class="pluh">
+<div class="gridthing">
+<h1 id="titlething" style="font-weight:700;">SLQNT IS IN THE FILES</h1>
 <div class="big-ep-list"> <!--release the files-->
-
+<a class="erm" href="https://gg.x.eyan.eu" target="_blank">
+<img src="/assets/img/astra.png" />
+</a>
+</div>
+</div>
 </div>
 </div>
 </body>
